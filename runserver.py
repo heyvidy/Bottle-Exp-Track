c769d692d8ee3f872a3a1fe85817ee90400b5ead
Vidyadhar <justvidyadhar@gmail.com>
2019-11-06 11:21:04 +0530

    Add models & db

--- runserver.py
+++ runserver.py
@@ -1,10 +1,27 @@
 # runserver.py
-from bottle import get, run, jinja2_view
+from bottle import get, run, jinja2_view, post, request, redirect
+from models import User, Expense, db
+
+db.connect()
 
 @get("/")
 @jinja2_view("home.html")
 def home():
 	return
+
+@post("/signup")
+def signup():
+	username = request.forms.get('username')
+	password = request.forms.get('password')
+
+	users = User.select().where(User.username == username)
+
+	if len(users) != 0:
+		return "User already exists!"
+	else:
+		User.create(username=username, password=password)
+	return redirect("/")
+
 
 run(host="localhost", 
 	port="8080", 
